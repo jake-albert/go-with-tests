58e065d356069325e10a23e0d1eede750cce0039
jake-albert <jakealbert93@gmail.com>
2023-10-14 20:03:13 +0900

    structs-methods-interfaces v1

--- structs-methods-interfaces/geometry_test.go
+++ structs-methods-interfaces/geometry_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -13,11 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		checkArea(t, rectangle, 100.0)
+	})
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
