293a1cf11354aa23c12823db8e01204718650417
jake-albert <jakealbert93@gmail.com>
2023-10-21 12:23:30 +0900

    maps v3

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -5,8 +5,9 @@ type Dictionary map[string]string
 // [NOTE] Can make custom errors constant by implementing Error method.
 // See: https://dave.cheney.net/2016/04/07/constant-errors
 const (
-	ErrNotFound   = DictionaryErr("could not find the word")
-	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+	ErrNotFound         = DictionaryErr("could not find the word")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
 )
 
 type DictionaryErr string
@@ -26,7 +27,7 @@ func (d Dictionary) Search(word string) (string, error) {
 
 // [NOTE] Maps values are pointers, so when passing by value the pointer is copied.
 // See: https://dave.cheney.net/2017/04/30/if-a-map-isnt-a-reference-variable-what-is-it
-func (d Dictionary) Add(word string, definition string) error {
+func (d Dictionary) Add(word, definition string) error {
 	_, err := d.Search(word)
 
 	switch err {
@@ -35,6 +36,20 @@ func (d Dictionary) Add(word string, definition string) error {
 		return nil
 	case nil:
 		return ErrWordExists
+	default:
+		return err
+	}
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+		return nil
 	default:
 		return err
 	}
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,13 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	word := "test"
+	definition := "definition"
+	dictionary := Dictionary{word: definition}
 
 	t.Run("known word", func(t *testing.T) {
-		got, err := dictionary.Search("test")
-		want := "this is just a test"
+		got, err := dictionary.Search(word)
+		want := definition
 
 		assertNoError(t, err)
 		assertStrings(t, got, want)
@@ -25,22 +27,44 @@ func TestSearch(t *testing.T) {
 func TestAdd(t *testing.T) {
 	t.Run("known word", func(t *testing.T) {
 		dictionary := Dictionary{}
-		definition := "this is just a test"
+		definition := "definition"
 		word := "test"
 
-		err := dictionary.Add(word, "this is just a test")
+		err := dictionary.Add(word, definition)
 
 		assertNoError(t, err)
 		assertDefinition(t, dictionary, word, definition)
 	})
 	t.Run("existing word", func(t *testing.T) {
 		word := "test"
-		definition := "this is just a test"
+		definition := "definition"
 		dictionary := Dictionary{word: definition}
-		err := dictionary.Add(word, "new test")
+		err := dictionary.Add(word, "new definition")
 
 		assertError(t, err, ErrWordExists)
 		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		dictionary := Dictionary{word: "old definition"}
+		definition := "new definition"
+
+		err := dictionary.Update(word, definition)
+
+		assertNoError(t, err)
+		assertDefinition(t, dictionary, word, definition)
+	})
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "definition"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
 	})
 }
 
