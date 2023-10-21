617be57bd40ae051cb12f4ff5c7125fee67aba88
jake-albert <jakealbert93@gmail.com>
2023-10-21 17:47:14 +0900

    select v1

--- select/racer.go
+++ select/racer.go
@@ -2,21 +2,26 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	durationA := measureResponseTime(a)
-	durationB := measureResponseTime(b)
-
-	if durationA < durationB {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	startA := time.Now()
-	http.Get(url)
-	return time.Since(startA)
+// [NOTE] struct{} is smaller even than bool in Go and seems to require no memory allocation
+// See: https://dave.cheney.net/2014/03/25/the-empty-struct
+func ping(url string) chan struct{} {
+	// [NOTE] Creating a channel with `var ch chan struct{}` leads to the "zero" value, nil.
+	// See: https://go.dev/play/p/IIbeAox5jKA
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
