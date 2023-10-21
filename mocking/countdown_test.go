e53d10f82e36a42defac01c5123d7208c09b5600
jake-albert <jakealbert93@gmail.com>
2023-10-21 15:11:30 +0900

    mocking v4

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 type SpyCountdownOperations struct {
@@ -60,4 +61,24 @@ Go!`
 			t.Errorf("got calls %v, want %v", spyCountdownOperations.Calls, want)
 		}
 	})
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
