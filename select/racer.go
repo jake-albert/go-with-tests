0ca625dfb0e4f6232605f2c4aaf082367afc02b1
jake-albert <jakealbert93@gmail.com>
2023-10-21 17:33:06 +0900

    select v0

---
+++ select/racer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	durationA := measureResponseTime(a)
+	durationB := measureResponseTime(b)
+
+	if durationA < durationB {
+		return a
+	}
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	startA := time.Now()
+	http.Get(url)
+	return time.Since(startA)
+}
